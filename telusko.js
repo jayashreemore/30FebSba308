cf51283e1b4cf12b1d177db08c8586d393db2262
jayashreemore <jayashreematre@gmail.com>
2024-03-03 22:09:02 -0500

    This is SBA308 Javascript Fundamentals

---
+++ telusko.js
@@ -0,0 +1,77 @@
+// objects by TELUSKO 
+
+// let alien = {
+//     name: 'Jaya',
+//     tech: 'Javascript',
+//     'work-exp': 4
+// }
+// console.log(alien['name']);
+// console.log(alien.tech);
+// console.log(alien['work-exp']);
+//console.log(alien[input]);
+
+//Complex Object by Telusko 
+/*
+let alien = {
+    name: 'Jaya',
+    tech: 'Javascript',
+    laptop: {
+        cpu : 'i7',
+        ram : 4,
+        brand1: 'asus'
+    }
+}
+
+
+//console.log(alien.laptop.brand.length); 
+//console.log(alien.laptop.brand?.length);
+
+delete alien.tech // to delete element from object 
+console.log(alien);
+*/
+//for loop in objets
+/*
+let alien = {
+    name: 'Jaya',
+    tech: 'Javascript',
+    laptop: {
+        cpu : 'i7',
+        ram : 4,
+        brand1: 'asus'
+    }
+}
+
+for (let key in alien) {
+    //console.log(key, alien.tech);//...this ll add js to all keys jaya js js js laptop js
+    console.log(key, alien[key]);
+}
+*/
+//functions
+/*
+function greet() // define a function 
+{
+    console.log("Hello World");
+}
+
+greet(); // calling a function 5 time 
+console.log("1");
+greet();
+greet();
+greet();
+greet();
+*/
+
+// function returns a value , pass a value 
+/*
+ function greet(user)
+ {
+    return `Hello ${user}!!`///backteak got pass the user name but coat ll print only user 
+ }
+
+ let user = 'Jaya';
+ let str = greet(user);
+ console.log(str);
+ */
+
+ //
+
